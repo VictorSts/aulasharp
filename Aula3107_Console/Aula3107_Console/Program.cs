42bcfdbb5fb4fa74e7db2812f80bdaf8d74db547
Victor Sts <ghosttop10@hotmail.com>
2018-08-01 09:42:56 -0300

    Adicionando o número

---
+++ Aula3107_Console/Aula3107_Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3107_Console
+{
+    class Program
+    {
+        public static int Id { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Digite um número:");
+            Id=int.Parse(Console.ReadLine());
+            ImprimirNumeroDigitado(Id);
+            Console.ReadKey();
+        }
+
+        static void ImprimirNumeroDigitado(int num)
+        {
+            Console.WriteLine("Você digitou: " + num);
+        }
+    }
+}
