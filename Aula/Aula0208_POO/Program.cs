27788c8b3321e79e5f697cc7a839523e57bba53d
Victor Sts <ghosttop10@hotmail.com>
2018-08-08 10:22:44 -0300

    Veículos

---
+++ Aula/Aula0208_POO/Program.cs
@@ -0,0 +1,47 @@
+﻿using Aula0208_POO.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula0208_POO
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Carro car = new Carro();
+                car.Modelo = "Ferrari";
+                car.Acelerar();
+                ExecutaTesteMotor(car);
+
+                Console.WriteLine("\n");
+
+                Moto moto = new Moto();
+                moto.Acelerar();
+                ExecutaTesteMotor(moto);
+
+                Console.WriteLine("\n");
+
+                Barco barco = new Barco();
+                barco.Acelerar();
+                ExecutaTesteMotor(barco);
+
+                Console.ReadKey();
+            }
+
+            static void ExecutaTesteMotor(IMotorizado vec)
+            {
+                Console.WriteLine("Velocidade atual: " + vec.Velocidade);
+                vec.Acelerar();
+                vec.Acelerar();
+                vec.Acelerar();
+                Console.WriteLine("Velocidade atual: " + vec.Velocidade);
+                vec.Frenar();
+                vec.Frenar();
+                Console.WriteLine("Velocidade atual: " + vec.Velocidade);
+            }
+        
+    }
+}
