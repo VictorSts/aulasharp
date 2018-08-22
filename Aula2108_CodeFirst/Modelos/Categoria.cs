55ac76ca17fa28e5e6c5b12bc8f67496743bd672
Victor Sts <car.victor@hotmail.com>
2018-08-21 21:29:28 -0300

    Criando uma Categoria

---
+++ Aula2108_CodeFirst/Modelos/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2108_CodeFirst.Modelos
+{
+    public class Categoria
+    {
+
+
+        public int CategoriaID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
