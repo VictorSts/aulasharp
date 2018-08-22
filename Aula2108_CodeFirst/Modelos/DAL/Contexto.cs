e7ba447ba84f34d9edd25d6b3809468246fc17f6
Victor Sts <car.victor@hotmail.com>
2018-08-21 22:16:49 -0300

    Classe Contexto e App.config

--- Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
+++ Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -1,7 +1,27 @@
-﻿namespace Aula2108_CodeFirst.Modelos.DAL
+﻿using System.Data.Entity;
+
+namespace Aula2108_CodeFirst.Modelos.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public Contexto() : base("nomeStringConexao")
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
 
     }
 }
+/**
+String conexão
+
+         -nome
+         -Data Source (servidor)
+String   -Initial Catalog (base de dados)
+         -Integrated Security=True
+     
+*/
